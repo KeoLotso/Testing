b33b12bccbe14937fde7fad04070187d80ded5c6
Keo.cs <147169034+KeoLotso@users.noreply.github.com>
2025-08-10 23:16:54 +0200

    Update oauth.js

--- api/oauth.js
+++ api/oauth.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: new URLSearchParams({
-        client_id: '1380895640556408862',
+        client_id: '1404211875775909958',
         client_secret: process.env.CLIENT_SECRET,
         grant_type: 'authorization_code',
         code,
@@ -31,4 +31,5 @@ export default async function handler(req, res) {
     return res.status(500).json({ success: false, error: 'Internal server error' });
   }
 }
+
 
